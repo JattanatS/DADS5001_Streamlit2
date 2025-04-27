b8549c3e475fa66fbd24630afce71c6899b16309
JattanatS <gh.jattanats@gmail.com>
2025-04-27 22:33:54 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -2,17 +2,13 @@ import streamlit as st
 
 def intro():
     import streamlit as st
-    #import duckdb as db
     import pandas as pd
     st.write("# Welcome to Streamlit! 👋")
     #st.sidebar.success("Select a demo above.")
 
     df = pd.read_csv("Starbucks.csv")
     st.write(df)
-    #x = 'Bangkok'
     x = st.sidebar.selectbox('Choose your city',['Bangkok','Ajman','Phuket'])
-    #results = db.sql(f"SELECT * FROM df where City='{x}'").df()
-    #st.write(results)
 
     st.markdown(
         """
